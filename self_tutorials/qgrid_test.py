53e071ebd759e4e97222e70a6f0b95662f9422b8
Alex <alex.chuang2718@gmail.com>
2020-07-31 11:36:57 -0400

    cleaning up unused files

--- self_tutorials/qgrid_test.py
+++
@@ -1,9 +0,0 @@
-import qgrid
-import pandas as pd
-import time
-
-df = pd.DataFrame({'col1': ['test1', 'test2'], 'col2': [3, 7]})
-qgrid_widget = qgrid.show_grid(df)
-display(qgrid_widget)
-time.sleep(10)
-qgrid_widget.get_changed_df()
