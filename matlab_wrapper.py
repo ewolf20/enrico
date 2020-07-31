af43383f2212330c690a72760852e3f127482c6e
Alex <alex.chuang2718@gmail.com>
2020-07-31 11:29:05 -0400

    added jpeg export to wrapper
    For quick previews on OD images from log editor. still need to modify MATLAB function to export OD image

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -1,7 +1,15 @@
+from PIL import Image
+import numpy as np
+
+
+def numpyfy_MATLABarray(matlab_array):
+    return np.array(matlab_array._data).reshape(matlab_array.size, order='F')
+
+
 def getMATLABanalysis(eng,
                       filepath,
 					  analysis_library_path=r'C:\Users\FermiCam2\Desktop\MatlabAnalysis\Fermi1_MatlabImageAnalysis',
-                     marqueeBox=None, normBox=None):
+                     marqueeBox=None, normBox=None, save_jpg_preview=True):
 
 	try:
 		eng.eval(r'cd ' + analysis_library_path, nargout=0)
@@ -9,6 +17,13 @@ def getMATLABanalysis(eng,
             matlab_dict = eng.getMeasNaAnalysis(filepath) #calling MATLAB function getMeasNaAnalysis
         else:
             matlab_dict = eng.getMeasNaAnalysis(filepath, 'marqueeBox', marqueeBox, 'normBox', normBox)
+
+        if save_jpg_preview and 'ODimage' in matlab_dict:
+        	np_im = numpyfy_MATLABarray(matlab_dict['ODimage'])
+        	im = Image.fromarray(np_im)
+        	save_filepath = filepath.replace('.spe', '.jpeg')
+        	im.save(save_filepath)
+        
 		return matlab_dict
 	except:
 		print('check if the Carsten MATLAB code is at the path specified\n')
