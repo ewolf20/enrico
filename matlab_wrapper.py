ce504a8dbe7770ff7094771f295d60eebcf26259
crobens <CRobens@MIT.edu>
2020-08-05 11:02:42 -0400

    fixed minor bugs

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -6,27 +6,22 @@ def numpyfy_MATLABarray(matlab_array):
     return np.array(matlab_array._data).reshape(matlab_array.size, order='F')
 
 
-def getMATLABanalysis(eng,
-                      filepath,
-					  analysis_library_path=r'C:\Users\FermiCam2\Desktop\MatlabAnalysis\Fermi1_MatlabImageAnalysis',
-                     marqueeBox=None, normBox=None, save_jpg_preview=True):
-
-	try:
-		eng.eval(r'cd ' + analysis_library_path, nargout=0)
-		if marqueeBox is None and normBox is None:
+def getMATLABanalysis(eng,filepath,
+                      analysis_library_path=r'C:\Users\FermiCam2\Desktop\MatlabAnalysis\Fermi1_MatlabImageAnalysis',
+                      marqueeBox=None, normBox=None, save_jpg_preview=True):
+    try:
+        eng.eval(r'cd ' + analysis_library_path, nargout=0)
+        if marqueeBox is None and normBox is None:
             matlab_dict = eng.getMeasNaAnalysis(filepath) #calling MATLAB function getMeasNaAnalysis
         else:
             matlab_dict = eng.getMeasNaAnalysis(filepath, 'marqueeBox', marqueeBox, 'normBox', normBox)
 
         if save_jpg_preview and 'ODimage' in matlab_dict:
-        	np_im = numpyfy_MATLABarray(matlab_dict['ODimage'])
-        	im = Image.fromarray(np_im)
-        	save_filepath = filepath.replace('.spe', '.jpeg')
-        	im.save(save_filepath)
+            np_im = numpyfy_MATLABarray(matlab_dict['ODimage'])
+            im = Image.fromarray(np_im)
+            save_filepath = filepath.replace('.spe', '.jpeg')
+            im.save(save_filepath)
         
-		return matlab_dict
-	except:
-		print('check if the Carsten MATLAB code is at the path specified\n')
-		print('check that an images exists at the filepath given')
-		
-		
+        return matlab_dict
+    except:
+        print('matlab wrapper error')
