90adf497955ccd6bcdca88f18ca6094cb8b62210
Alex <alex.chuang2718@gmail.com>
2020-08-07 11:27:29 -0400

    add enrico_bot and bug fixes in image_watchdog
    image_watchdog now moves files to misplaced images folder when new run_id cannot be fetched and continues running, and enrico_bot posts to Slack.

---
+++ enrico_bot.py
@@ -0,0 +1,34 @@
+import json
+import slack
+
+with open('fermi1bot_config.json') as json_file:
+    data = json.load(json_file)
+    token = data['token']
+
+client = slack.WebClient(token=token)
+
+
+def post_message(message):
+    try:
+        response = client.chat_postMessage(
+            channel='#enrico_notifications',
+            text=message)
+        return response
+    except:
+        print('enrico_bot error')
+        pass
+
+
+def post_image(image_filepath):
+    try:
+        response = client.files_upload(
+            file=image_filepath,
+            channels='#enrico_notifications'
+        )
+        return response
+    except:
+        print('enrico_bot error')
+        pass
+
+
+post_image('1_Enrico-Fermi.jpg')
