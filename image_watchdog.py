d70450bb09871f0c5ee9c7f247f4735d21284dff
Alex <alex.chuang2718@gmail.com>
2020-08-10 18:21:47 -0400

    added uploading to breadboard to image_watchdog
    - removed local logging
    - images are now named by run_id after moving from watchfolder
    - removed local logging

--- image_watchdog.py
+++ image_watchdog.py
@@ -9,6 +9,8 @@ python3 breadboard_python_watchdog.py [WATCHFOLDER]
 where [WATCHFOLDER] is the folder your camera program writes images to.
 
 '''
+# import latest version of breadboard from github, rather than using the pip install.
+breadboard_repo_path = r'C:\Users\Alex\Dropbox (MIT)\lab side projects\control software\breadboard-python-client\\'
 
 import os
 import time
@@ -16,6 +18,7 @@ import datetime
 import shutil
 import posixpath
 import sys
+sys.path.insert(0, breadboard_repo_path)
 from breadboard import BreadboardClient
 import warnings
 import pandas as pd
@@ -55,6 +58,15 @@ def get_newest_run_dict():
                                   **new_run_dict['parameters']}
             return new_run_dict_clean, run_id_guess - 1
         run_id_guess += 1
+
+
+def check_run_image_concurrent(runtime_str, max_time_diff_in_sec=40):
+    runtime = datetime.datetime.strptime(runtime_str, "%Y-%m-%dT%H:%M:%SZ")
+    time_diff = (datetime.datetime.today() - runtime)
+    if time_diff.total_seconds() < max_time_diff_in_sec:
+        return True
+    else:
+        return False
 
 
 def rename_file(filename):
@@ -117,8 +129,42 @@ def main():
                 # waiting for all images from  newest run
                 continue
             else:
+                new_row_dict, _ = get_newest_run_dict()
+
+                if new_row_dict['run_id'] == old_run_id and old_run_id is not None:
+                    get_dict_tries, max_tries = 0, 20
+                    while get_dict_tries < max_tries:
+                        new_row_dict, _ = get_newest_run_dict()
+                        if new_row_dict['run_id'] != old_run_id:
+                            break
+                        else:
+                            get_dict_tries += 1
+                            time.sleep(0.2)
+                    if new_row_dict['run_id'] == old_run_id:
+                        warnings.warn(
+                            'run_id did not update between shots, check on control PC if cicero breadboard logger is on.')
+                        if not warned:  # prevent enrico_bot from spamming
+                            enrico_bot.post_message(
+                                'Image watchdog could not get new unique run_id for new image. Check the experiment.')
+                            warned = True
+                        move_misplaced_images()
+                        continue
+    #             if old_list_bound_variables is not None:
+    #                 if set(new_row_dict['ListBoundVariables']) != set(old_list_bound_variables):
+    #                     warnings.warning('List bound variables changed in Cicero. Check if new run should be started.')
+                if not check_run_image_concurrent(new_row_dict['runtime']):
+                    if not warned:
+                        enrico_bot.post_message(
+                            'Incoming image and latest Breadboard runtime differ by too much. Assign run_id manually later.')
+                        warned = True
+                    warnings.warn(
+                        'Incoming image and latest Breadboard runtime differ by too much. Assign run_id manually later.')
+                    move_misplaced_images()
+                    continue
+
                 output_filenames = []
                 new_names = sorted(new_names)
+                image_idx = 0
                 for filename in new_names[0:n_images_per_run]:
                     print(filename)
                     done_moving = False
@@ -134,88 +180,62 @@ def main():
                                     old_filesize = os.path.getsize(
                                         os.path.join(r'images\\', filename))
                                     time.sleep(0.2)
-                            # prevent overwriting data
+                            # rename images according to their associated run_id
                             old_filename = filename
+                            new_filename = str(run_id) + \
+                                '_' + str(image_idx) + '.spe'
                             new_filepath = os.path.join(
-                                measurement_dir, filename)
+                                measurement_dir, new_filename)
                             if os.path.exists(new_filepath):
-                                filename = rename_file(filename)
+                                new_filename = rename_file(new_filename)
                                 new_filepath = os.path.join(
-                                    measurement_dir, filename)
+                                    measurement_dir, new_filename)
             #                     if save_to_BEC1_server:
             #                         new_filepath_BEC1server = 'foo' #TODO set BEC1 server filepath
             #                         shutil.copyfile(os.path.join(r'images\\', old_filename), new_filepath_BEC1server)
                             shutil.move(os.path.join(
                                 r'images\\', old_filename), new_filepath)
                             done_moving = True
-                            output_filenames.append(filename)
+                            image_idx += 1
+                            output_filenames.append(new_filename)
                         except:
                             time.sleep(0.5)
 
             # associate new image with latest run parameters in local log
 
-            new_row_dict, _ = get_newest_run_dict()
-            print('run id: \n')
-            print(new_row_dict['run_id'])
-            if new_row_dict['run_id'] == old_run_id and old_run_id is not None:
-                get_dict_tries, max_tries = 0, 20
-                while get_dict_tries < max_tries:
-                    new_row_dict, _ = get_newest_run_dict()
-                    if new_row_dict['run_id'] != old_run_id:
-                        break
-                    else:
-                        get_dict_tries += 1
-                        time.sleep(0.2)
-                if new_row_dict['run_id'] == old_run_id:
-                    warnings.warn(
-                        'run_id did not update between shots, check on control PC if cicero breadboard logger is on.')
-                    if not warned:  # prevent enrico_bot from spamming
-                        enrico_bot.post_message(
-                            'Image watchdog could not get new unique run_id for new image. Check the experiment.')
-                        warned = True
-                    move_misplaced_images()
-                    continue
-#             if old_list_bound_variables is not None:
-#                 if set(new_row_dict['ListBoundVariables']) != set(old_list_bound_variables):
-#                     warnings.warning('List bound variables changed in Cicero. Check if new run should be started.')
             old_run_id = new_row_dict['run_id']
-            for filenum in range(n_images_per_run):
-                new_row_dict['filename' +
-                             str(filenum)] = output_filenames[filenum]
-            new_row_dict['notes'] = ' '
-            new_row_dict['badshot'] = False
-            if df_log is None:
-                for key in new_row_dict.keys():
-                    new_row_dict[key] = [new_row_dict[key]]
-                df_log = pd.DataFrame(new_row_dict)
-            else:
-                if os.path.exists(log_filepath):
-                    df_log = pd.read_csv(log_filepath)
-                df_log = df_log.append(new_row_dict, ignore_index=True)
-            df_log.to_csv(log_filepath, index=False)
+            # for filenum in range(n_images_per_run):
+            #     new_row_dict['filename' +
+            #                  str(filenum)] = output_filenames[filenum]
+            # new_row_dict['notes'] = ' '
+            # new_row_dict['badshot'] = False
+            # df_log = pd.DataFrame({key:[new_row_dict[key]] for key in new_row_dict})
+            # # write to local log
+            # if df_log is None:
+            #     df_log.to_csv(log_filepath, index=False)
+            # else:
+            #     df_log.to_csv(log_filepath, index=False, mode='a')
 
-            # # Write to Breadboard
-            # try:
-            #     resp = bc.post_images(
-            #                 image_names = os.path.splitext(output_filename)[0],
-            #                 filepath = output_filepath,
-            #                 image_times = [dt],
-            #                 auto_time = False # Add more information here
-            #                 )
-            #     if resp.status_code!=200:
-            #         print(resp.text)
-            # except: pass
-            # names_old = names
-            names_old, _ = getFileList(watchfolder)
-            print('New file(s): ' + str(output_filenames) +
-                  ' associated with run parameters \n')
-            print(new_row_dict['runtime'])
-            listboundvars = new_row_dict['ListBoundVariables']
+            # Write to Breadboard
             try:
-                for var in listboundvars:
-                    print(var + ':' + str(new_row_dict[var]))
+                resp = bc.append_images_to_run(
+                    new_row_dict['run_id'], output_filenames)
+                bc.add_measurement_name_to_run(
+                    new_row_dict['run_id'], measurement_dir)
+                if resp.status_code != 200:
+                    print(resp.text)
             except:
-                print('bug: first shot formatting issues')
+                warnings.warn('Failed to write to breadboard.')
+                pass
+            # print('New file(s): ' + str(output_filenames) +
+            #       ' associated with run parameters \n')
+            # print(new_row_dict['runtime'])
+            # listboundvars = new_row_dict['ListBoundVariables']
+            # try:
+            #     for var in listboundvars:
+            #         print(var + ':' + str(new_row_dict[var]))
+            # except:
+            #     print('bug: first shot formatting issues')
 
             old_list_bound_variables = new_row_dict['ListBoundVariables']
 
