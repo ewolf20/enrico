388348b89d7b28e0139a2ed870f30a8165dcea6d
Alex <alex.chuang2718@gmail.com>
2020-08-13 11:53:59 -0400

    consolidate code from analysis_logger and MATLAB wrappers in .py files

---
+++ analysis_logger.py
@@ -0,0 +1,129 @@
+breadboard_repo_path = r'D:\Fermidata1\enrico\breadboard-python-client\\'
+import ipywidgets as widgets
+from ipywidgets import interact
+from measurement_directory import *
+import sys
+sys.path.insert(0, breadboard_repo_path)
+from breadboard import BreadboardClient
+# enter your path to the API_config
+bc = BreadboardClient(config_path='API_CONFIG_fermi1.json')
+import os
+import time
+import datetime
+import shutil
+import warnings
+import enrico_bot
+from image_watchdog import getFileList
+
+
+# loading takes a while, don't run this if testing
+def load_matlab_engine():
+    print('loading matlab engine...')
+    import matlab.engine
+    eng = matlab.engine.start_matlab()
+    print('matlab engine loaded')
+    return eng
+
+
+warnings.filterwarnings(
+    "ignore", "Your application has authenticated using end user credentials")
+warnings.filterwarnings(
+    "ignore", "Could not find appropriate MS Visual C Runtime")
+bc = BreadboardClient(config_path='API_CONFIG_fermi1.json')
+
+
+def main(analysis_type, watchfolder, load_matlab=True):
+    refresh_time = 1  # seconds
+    print("\n\n Watching this folder for changes: " + watchfolder + "\n\n")
+
+    # loading matlab takes a while, disable if testing
+    if load_matlab:
+        eng = load_matlab_engine()
+
+    # define subfunction analysis_function which outputs analysis and settings dictionaries
+    # define analyzed_var_names list manually, these are the scalar values most easily parsed from Carsten's analysis MATLAB structs.
+    # TODO: parse all of Carsten's MATLAB struct, not just scalar values.
+    if analysis_type == 'fake analysis':
+        import matlab_wrapper.fake_analysis1 as analysis_function
+        import matlab_wrapper.fake_analysis1_var_names as analyzed_var_names
+    elif analysis_type == 'fake analysis 2':
+        import matlab_wrapper.fake_analysis2 as analysis_function
+        import matlab_wrapper.fake_analysis2_var_names as analyzed_var_names
+    elif analysis_type == 'ycam':
+        from matlab_wrapper import getYcamAnalysis
+        import matlab_wrapper.ycam_analyzed_var_names as analyzed_var_names
+
+        def analysis_function(filepath, previous_settings=None):
+            if previous_settings is None:
+                matlab_dict = getMATLABanalysis(eng, filepath)
+            else:
+                matlab_dict = getMATLABanalysis(eng, filepath, marqueeBox=previous_settings['marqueeBox'],
+                                                normBox=previous_settings['normBox'])
+            analysis_dict, settings = matlab_dict['analysis'], matlab_dict['settings']
+            return analysis_dict, settings
+    elif analysis_type == 'zcam_dual_imaging':
+        from matlab_wrapper import getDualImagingAnalysis
+        import matlab_wrapper.dual_imaging_analyzed_var_names as analyzed_var_names
+
+        def analysis_function(filepath, previous_settings=None):
+            if previous_settings is None:
+                matlab_dict = getDualImagingAnalysis(eng, filepath)
+    #         else:
+    #             matlab_dict = getMATLABanalysis(eng, filepath, marqueeBox = previous_settings['marqueeBox'],
+    #                                            normBox = previous_settings['normBox'])
+            analysis_dict = matlab_dict['analysis']
+            settings = None
+            return analysis_dict, settings
+
+    # wrap analysis_function
+    def analyze_image(image_filename, previous_settings=None, output_previous_settings=True):
+        # TODO adapt for triple imaging later
+        abs_image_path = os.path.join(os.path.join(
+            os.getcwd(), watchfolder), image_filename)
+        print('{file} analyzing: '.format(file=image_filename))
+        analysis_dict, settings = analysis_function(
+            abs_image_path, previous_settings)
+        if not output_previous_settings:
+            settings = None  # forces user to select new marquee box for each shot
+        for key in analyzed_var_names:
+            print(key, analysis_dict[key])
+        return analysis_dict, settings
+
+    previous_settings = None
+    unanalyzed_files = []
+    done_files = []
+    append_mode = True
+
+    # Main Loop
+    while True:
+        if not os.path.exists(watchfolder):
+            time.sleep(refresh_time)
+            continue
+        else:
+            files, _ = getFileList(watchfolder)
+            fresh_files = sorted(list(set(files).difference(
+                set(done_files)).difference(set(unanalyzed_files))))
+            unanalyzed_files += fresh_files  # push newest files to top of stack
+
+        for file in reversed(unanalyzed_files):  # start from top of stack
+            run_id = run_id_from_filename(file)
+            if append_mode:
+                run_dict = bc._send_message(
+                    'get', '/runs/' + str(run_id) + '/').json()
+                if set(analyzed_var_names).issubset(set(run_dict.keys())):
+                    popped_file = [unanalyzed_files.pop()]
+                    done_files += popped_file
+                    continue
+            try:
+                analysis_dict, previous_settings = analyze_image(
+                    file, previous_settings)
+                popped_file = [unanalyzed_files.pop()]
+                done_files += popped_file
+            except:
+                analysis_dict = {}
+                warnings.warn(
+                    str(run_id) + 'could not be analyzed. Skipping for now.')
+            resp = bc.append_analysis_to_run(run_id, analysis_dict)
+            print('\n')
+
+        time.sleep(refresh_time)
--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -2,13 +2,18 @@
 
 from PIL import Image
 import numpy as np
+from random import randint
 
 
 def numpyfy_MATLABarray(matlab_array):
     return np.array(matlab_array._data).reshape(matlab_array.size, order='F')
 
+#getAnalysisModeAnalysis takes (matlab_engine, filepath, **kwargs) and returns a dictionary translated from a MATLAB analysis struct.
+#analysismode_analyzed_var_names is manually defined to include scalar values from the analysis dictionary. These are most easily written to breadboard.
 
-def getMATLABanalysis(eng,filepath,
+ycam_analyzed_var_names = ['bareNcntAverageMarqueeBoxValues',
+                              'COMX', 'COMY']
+def getYcamAnalysis(eng,filepath,
                       analysis_library_path=r'C:\Users\FermiCam2\Desktop\MatlabAnalysis\Fermi1_MatlabImageAnalysis',
                       marqueeBox=None, normBox=None, save_jpg_preview=True):
     try:
@@ -27,3 +32,45 @@ def getMATLABanalysis(eng,filepath,
         return matlab_dict
     except:
         print('matlab wrapper error')
+
+dual_imaging_analyzed_var_names = ['K_bareNcntAverageMarqueeBoxValues', 'Na_bareNcntAverageMarqueeBoxValues',
+                              'Na_COMX', 'Na_COMY']
+def getDualImagingAnalysis(eng, filepath,
+                           analysis_library_path=r'C:\Users\Fermi1\Documents\GitHub\Fermi1_MatlabImageAnalysis',
+                           marqueeBox=None, normBox=None, save_jpg_preview=True):
+    try:
+        eng.eval(r'cd ' + analysis_library_path, nargout=0)
+        matlab_dict = eng.getDualImagingZcamAnalysis(filepath)
+        # if marqueeBox is None and normBox is None:
+        #     matlab_dict = eng.getMeasNaAnalysis(filepath) #calling MATLAB function getMeasNaAnalysis
+        # else:
+        #     matlab_dict = eng.getMeasNaAnalysis(filepath, 'marqueeBox', marqueeBox, 'normBox', normBox)
+
+        if save_jpg_preview and 'ODimage' in matlab_dict:
+            np_im = numpyfy_MATLABarray(matlab_dict['ODimage'])
+            im = Image.fromarray(np_im)
+            save_filepath = filepath.replace('.spe', '.jpeg')
+            im.save(save_filepath)
+
+        flatten_dict = {'analysis': {}, 'settings' : {}}
+        for key in matlab_dict['K_analysis']:
+            flatten_dict['analysis']['K_' +
+                                     key] = matlab_dict['K_analysis'][key]
+        for key in matlab_dict['Na_analysis']:
+            flatten_dict['analysis']['Na_' +
+                                     key] = matlab_dict['Na_analysis'][key]
+        return flatten_dict #matlab struct object is passed to python as a dictionary, to be parsed in analysis_logger.py
+    except:
+        print('matlab wrapper error')
+
+##################################################################################################################################
+
+fake_analysis1_var_names = ['fake analysis 1', 'fake analysis 2']
+def fake_analysis1(filepath, previous_settings=None):
+    time.sleep(randint(0, 2))
+    return {'fake analysis 1': randint(0, 42), 'fake analysis 2': randint(0, 42)}, None
+
+fake_analysis2_var_names = ['fake analysis 3', 'fake analysis 4']
+def fake_analysis2(filepath, previous_settings=None):
+    time.sleep(randint(0, 5))
+    return {'fake analysis 3': randint(0, 42), 'fake analysis 4': randint(0, 42)}, None
