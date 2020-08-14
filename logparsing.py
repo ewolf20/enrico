b69210a49beedfc03f2ca9819926140c4168b545
achuang2718 <alex.chuang2718@gmail.com>
2020-08-14 18:43:42 -0400

    cleaning up some things

--- logparsing.py
+++
@@ -1,9 +0,0 @@
-# module for misc helper functions for parsing log files, MATLAB objects etc.
-
-def translate_stringy_list(stringy_list):
-    # pd.read_csv imports listboundvariables dtype as a string, we want it in list form
-    final_list = []
-    stringy_list = stringy_list.split(',')
-    for var in stringy_list:
-        final_list.append(var.translate({ord(c): None for c in '[ ]\,\''}))
-    return final_list
