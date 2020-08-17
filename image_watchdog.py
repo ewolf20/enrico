4487cf651d25ab6ed982e42b6f0401cab69917a2
Alex <alex.chuang2718@gmail.com>
2020-08-17 14:00:09 -0400

    added more details to debugging logger
    - also added safety_check_passed boolean to make safety check snippet more readable

--- image_watchdog.py
+++ image_watchdog.py
@@ -30,7 +30,8 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
-file_handler = logging.FileHandler('image_watchdog_debugging.log')
+file_handler = logging.FileHandler(measurement_directory(
+    measurement_name='') + 'image_watchdog_debugging.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
@@ -75,13 +76,14 @@ def get_newest_run_dict():
         run_id_guess += 1
 
 
-def check_run_image_concurrent(runtime_str, max_time_diff_in_sec=10, min_time_diff_in_sec=0):
+def check_run_image_concurrent(runtime_str, incomingfile_time, max_time_diff_in_sec=10, min_time_diff_in_sec=0):
     runtime = datetime.datetime.strptime(runtime_str, "%Y-%m-%dT%H:%M:%SZ")
-    time_diff = (datetime.datetime.today() - runtime)
+    time_diff = (incomingfile_time - runtime)
     if min_time_diff_in_sec < time_diff.total_seconds() < max_time_diff_in_sec:
         return True
     else:
-        print("time diff in seconds: {time_diff}".format(time_diff = str(time_diff.total_seconds())))
+        print("time diff in seconds: {time_diff}".format(
+            time_diff=str(time_diff.total_seconds())))
         return False
 
 
@@ -98,13 +100,14 @@ def rename_file(filename):
     return rename
 
 
-def main(measurement_name = None):
+def main(measurement_name=None):
     refresh_time = 1  # seconds
 
     """Name the set of runs"""
     print('existing runs: ')
     print(todays_measurements())
-    measurement_dir = measurement_directory(measurement_name = measurement_name, warn=True)
+    measurement_dir = measurement_directory(
+        measurement_name=measurement_name, warn=True)
 
     watchfolder = os.getcwd() + '\images'  # feed the program your watchfolder
 
@@ -145,20 +148,21 @@ def main(measurement_name = None):
             #     'new run_id: ' + str(new_row_dict['run_id']) + '. runtime: ' + str(new_row_dict['runtime']))
             # print('list bound variables: {run_dict}'.format(run_dict={key: new_row_dict[key]
             #                                                           for key in new_row_dict['ListBoundVariables']}))
-            logger.debug(
-                'new run_id: ' + str(new_row_dict['run_id']) + '. runtime: ' + str(new_row_dict['runtime']))
             logger.debug('list bound variables: {run_dict}'.format(run_dict={key: new_row_dict[key]
                                                                              for key in new_row_dict['ListBoundVariables']}))
+            logger.debug(
+                'new run_id: ' + str(new_row_dict['run_id']) + '. runtime: ' + str(new_row_dict['runtime']))
             displayed_run_id = new_row_dict['run_id']
 
         # check if new images has come in
         if len(new_names) > 0:
+            incomingfile_time = datetime.datetime.today()
             if len(new_names) < n_images_per_run:
                 # waiting for all images from  newest run
                 continue
             else:
-                # new_row_dict, _ = get_newest_run_dict()
-
+                # safety checks that run_id is updating and image came within 10 seconds of last Cicero upload.
+                safety_check_passed = True
                 if new_row_dict['run_id'] == old_run_id and old_run_id is not None:
                     get_dict_tries, max_tries = 0, 20
                     while get_dict_tries < max_tries:
@@ -167,29 +171,31 @@ def main(measurement_name = None):
                             break
                         else:
                             get_dict_tries += 1
-                            time.sleep(0.2)
+                            time.sleep(0.1)
                     if new_row_dict['run_id'] == old_run_id:
-                        warning_message = 'run_id did not update between shots, check on control PC if cicero breadboard logger is on.'
+                        warning_message = 'run_id did not update from old id {id} between shots, check on control PC if cicero breadboard logger is on.'.format(
+                            id=str(old_run_id))
                         warnings.warn(warning_message)
-                        # print(warning)
                         logger.warn(warning_message)
                         if not warned:  # prevent enrico_bot from spamming
                             enrico_bot.post_message(warning_message)
                             warned = True
-                        move_misplaced_images()
-                        continue
-    #             if old_list_bound_variables is not None:
-    #                 if set(new_row_dict['ListBoundVariables']) != set(old_list_bound_variables):
-    #                     warnings.warning('List bound variables changed in Cicero. Check if new run should be started.')
-                if not check_run_image_concurrent(new_row_dict['runtime']):
-                    warning_message = 'Incoming image and latest Breadboard runtime differ by too much. Assign run_id manually later.'
+                        safety_check_passed = False
+                if not check_run_image_concurrent(new_row_dict['runtime'], incomingfile_time) and safety_check_passed:
+                    warning_message = 'Incoming image time and latest Breadboard runtime differ by too much. Check run_id {id} manually later.'.format(
+                        id=str(new_row_dict['run_id']))
                     if not warned:
                         enrico_bot.post_message(warning_message)
                         warned = True
                     warning = warnings.warn(warning_message)
                     # print(warning)
                     logger.warn(warning_message)
-                    move_misplaced_images()
+                    safety_check_passed = False
+
+                if not safety_check_passed:
+                    logger.debug('moved {files} to {directory}'.format(
+                        files=str(os.listdir('images')), directory=measurement_dir + 'misplaced'))
+                    shutil.move(r'images', measurement_dir + 'misplaced')
                     continue
 
                 output_filenames = []
@@ -197,8 +203,6 @@ def main(measurement_name = None):
                 image_idx = 0
                 run_id = new_row_dict['run_id']
                 for filename in new_names[0:n_images_per_run]:
-                    # print('moving ' + filename)
-                    logger.debug('moving ' + filename)
                     done_moving = False
                     while not done_moving:
                         # prevent python from corrupting file, wait for writing to disk to finish
@@ -224,6 +228,8 @@ def main(measurement_name = None):
         #                         shutil.copyfile(os.path.join(r'images\\', old_filename), new_filepath_BEC1server)
                         shutil.move(os.path.join(
                             r'images\\', old_filename), new_filepath)
+                        logger.debug('moving {old_name} to {destination}'.format(old_name=old_filename,
+                                                                                 destination=new_filename))
                         done_moving = True
                         image_idx += 1
                         output_filenames.append(new_filename)
@@ -236,12 +242,15 @@ def main(measurement_name = None):
                 bc.add_measurement_name_to_run(
                     new_row_dict['run_id'], measurement_dir)
                 if resp.status_code != 200:
-                    # print(resp.text)
-                    logger.warn(resp.text)
+                    logger.warn('Upload error: ' + resp.text)
+                else:
+                    logger.debug('Uploaded filenames {files} to breadboard run_id {id}.'.format(
+                        files=str(output_filenames), id=str(new_row_dict['run_id'])))
             except:
-                warning = warnings.warn('Failed to write to breadboard.')
-                # print(warning)
-                logger.warn('Failed to write to breadboard.')
+                warning = 'Failed to write {files} to breadboard run_id {id}.'.format(
+                    files=str(output_filenames), id=str(new_row_dict['run_id']))
+                warnings.warn(warning)
+                logger.warn(warning)
                 pass
 
             old_list_bound_variables = new_row_dict['ListBoundVariables']
@@ -251,4 +260,10 @@ def main(measurement_name = None):
 
 
 # if __name__ == "__main__":
-#     main()
+#     # automatic_restart = None
+#     # while not (automatic_restart == 'y' or automatic_restart == 'n'):
+#     #     automatic_restart = input('Would you like image_watchdog to automatically restart and save to the folder MM/YYMMDD/automatic_restart after crash? Recommended for overnight runs only. [y/n]: ')
+#     try:
+#         main()
+#     except:
+#         pass
