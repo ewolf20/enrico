da541fc5545ad4584f8c6a0a4af79028a83b54a4
achuang2718 <alex.chuang2718@gmail.com>
2020-10-15 15:10:06 -0400

    Revert "finished testing BEC1server backup in image_watchdog.py"
    This reverts commit bcf63f7fe60eff51c8f78e672629e2a9673aa54a.

--- batch_shortcuts/watchdog_and_analysis.bat
+++ batch_shortcuts/watchdog_and_analysis.bat
@@ -4,9 +4,8 @@ CD /d C:\Users\Fermi1\anaconda3\Scripts
 CALL activate.bat
 
 CD /d D:\Fermidata1\enrico
-ECHO DID YOU CLOSE THE PREVIOUS INSTANCE OF image_watchdog.py?
-PAUSE
 START python image_watchdog.py
 ECHO image_watchdog.py started in a new window
 START python analysis_logger.py
 ECHO analysis_logger.py started in a new window
+PAUSE
